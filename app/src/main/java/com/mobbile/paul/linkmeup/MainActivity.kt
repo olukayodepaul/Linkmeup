be5fe422a46af04572d4c0bd1784fbed095c1806
olukayodepaul <paul.olukayode.pro@gmail.com>
2020-07-21 21:40:15 +0100

    Adding Hilt for Dependency Inject

--- app/src/main/java/com/mobbile/paul/linkmeup/MainActivity.kt
+++ app/src/main/java/com/mobbile/paul/linkmeup/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mobbile.paul.linkmeup
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+}
+
+class conInjector
+ @Inject
+ constructor(private val intefaceImp: IntefaceImp){
+    fun getThing(): String {
+        return intefaceImp.getIntefaceObject()
+    }
+}
+
+interface someInterface {
+    fun getInterface(): String
+    fun getIntefaceObject(): String
+}
+
+class IntefaceImp
+ constructor(): someInterface{
+    override fun getInterface(): String {
+        return ""
+    }
+
+    override fun getIntefaceObject(): String {
+        return ""
     }
 }
 
