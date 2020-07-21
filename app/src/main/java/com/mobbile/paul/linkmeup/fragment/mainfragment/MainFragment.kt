461b377c59a452e95af0323e16b5696e64e41d42
olukayodepaul <paul.olukayode.pro@gmail.com>
2020-07-21 21:55:09 +0100

    Adding Fragment

--- app/src/main/java/com/mobbile/paul/linkmeup/fragment/mainfragment/MainFragment.kt
+++ app/src/main/java/com/mobbile/paul/linkmeup/fragment/mainfragment/MainFragment.kt
@@ -1,2 +1,28 @@
 package com.mobbile.paul.linkmeup.fragment.mainfragment
 
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+
+class MainFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+}
